4a37bbc036befeced4cc231ec7994b4f7babbfeb
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-09-10 01:52:34 -0500

    added tests

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -12,7 +12,7 @@ function Modal({ onClose, currentPhoto }) {
           alt="current category"
         />
         <p>{description}</p>
-        <button onClick={onClose} type="button">
+        <button data-testid="buttontag" onClick={onClose} type="button">
           Close this modal
         </button>
       </div>
